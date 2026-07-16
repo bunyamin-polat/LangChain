dfaa6dea479306b8294cf2f71d80d2a76e790cb7
bunyamin-polat <muhendisbp@gmail.com>
2026-07-16 21:15:10 +0200

    enviroment setup

---
+++ projects/hello_world/main.py
@@ -0,0 +1,14 @@
+import os
+
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+
+def main():
+    print("Hello from hello-world!")
+
+
+if __name__ == "__main__":
+    main()
